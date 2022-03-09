2d38ebc64b0b05e27025922641fb15976650dac6
moamlrh <moamll288@gmail.com>
2022-03-09 13:41:43 +0300

    gitginore config

--- firstapp.c
+++ firstapp.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include  <stdio.h>
+#include <time.h>
 
-void main()
+void swap(int* a, int* b){
+    printf("\nswaping...\n");
+    sleep(1);
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(int argc, char const *argv[])
 {
-    int age = 21; // integer varialbe;
-    int *pAge; // initiliaze the pointer;
-
-    pAge = &age; // assign the position of age var to the pAge pointer;
-
-
-    printf("%p\n", pAge); // print the pointer "position of age in hexadecimal"
-    printf("%d\n", *pAge); // deserialization of pointer or  print the value of 
+    int a =1, b=3;
+    printf("\nbefore\nvalue of a is %d\nvalue of b is %d\n", a,b);
+    swap(&a,&b);
+    printf("\nafter\nvalue of a is %d\nvalue of b is %d\n", a,b);
+    return 0;
 }
