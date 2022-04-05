5cae20000e42176e6381e522d1b8c00c9cfd3630
moamlrh <moamll288@gmail.com>
2022-04-05 20:11:05 +0300

    added description to static keyword example

--- static-word.c
+++ static-word.c
@@ -2,6 +2,13 @@
     when we use static with a variable inside a function it's mean the value of var 
     it's won't change every time we call the function,
     try to delete the static from the example below and see the difference. 
+
+
+    1- A static variable inside a function keeps its value between invocations.
+    2- A static global variable or a function is "seen" only in the file it's declared in
+
+    you can create function with static keyword that's mean 
+    the you can't use the function in other files 
 */
 
 #include "stdio.h"
@@ -13,6 +20,8 @@ void func()
     printf("%d\n", x);
     x++;
 }
+
+
 int main(int argc, char const *argv[])
 {
     func(); // x = 10
